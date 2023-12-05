510eb34af7e3e7196e49c83a767ee440868d8dc1
Asif Ihsan <asifihsan0369@gmail.com>
2023-12-05 19:33:25 +0530

    Square Root Added

--- calculator.py
+++ calculator.py
@@ -1,8 +1,13 @@
+import math
 def addtion():
     n1=int(input("Enter the 1st number: "))
     n2=int(input("Enter the 2nd number: "))
     sum=n1+n2
     print("The sum is : ",sum)
+def sqrt():
+    n=int(input("Enter a number\n"))
+    s=math.sqrt(n)
+    print("Square root of ",n," is ",s)
 
 print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Square-Root\n6.Power\n7.exit\n")
 op=0
@@ -18,8 +23,11 @@ while op<7:
         case 4:
             print("Work In Progress")
         case 5:
-            print("Work In Progress")
+            sqrt()
         case 6:
             print("Work In Progress")
+        case 7:
+            print("Thank You!")
         case default:
             print("Enter a valid option..")
+            op=0
