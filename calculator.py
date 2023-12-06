02d3b7426349a5792765e21c843a04be7a9915b5
am-niz <139251623+am-niz@users.noreply.github.com>
2023-12-06 18:59:39 +0530

    Changes in Ashitha calculator.py

--- calculator.py
+++ calculator.py
@@ -9,20 +9,16 @@ def sqrt():
     n=int(input("Enter a number\n"))
     s=math.sqrt(n)
     print("Square root of ",n," is ",s)
-<<<<<<< HEAD
 def multiplication():
     n1=int(input("Enter the 1st number: "))
     n2=int(input("Enter the 2nd number: "))
     mul=n1+n2
     print("The multiplication is : ",mul)
-
-=======
 def div():
     n1=int(input("Enter the 1st number: "))
     n2=int(input("Enter the 2nd number: "))
     d=n1/n2
-    print("The sum is : ",d)
->>>>>>> eb3528de1269177ede4dcc7828517864668a7073
+    print("The sum is : ",d")
 print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Square-Root\n6.Power\n7.exit\n")
 op=0
 while op<7:
