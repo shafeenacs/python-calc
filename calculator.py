7ad7fa96e0b82f1c92a2e333ac55a3026be38b55
alen46 <52160261+alen46@users.noreply.github.com>
2023-12-06 18:56:36 +0530

    Added feature subtraction

--- calculator.py
+++ calculator.py
@@ -13,6 +13,12 @@ def div():
     n2=int(input("Enter the 2nd number: "))
     d=n1/n2
     print("The sum is : ",d)
+def subtraction():
+    a=int(input("Enter 1st number"))
+    b=int(input("Enter 2nd number"))
+    d=a-b
+    print("Difference = ",d)
+    return
 print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Square-Root\n6.Power\n7.exit\n")
 op=0
 while op<7:
@@ -21,7 +27,7 @@ while op<7:
         case 1:
             addtion()
         case 2:
-            print("Work In Progress")
+            subtraction()
         case 3:
             print("Work In Progress")
         case 4:
