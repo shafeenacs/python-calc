2efdc545f4dd4145804527e95d63cfcb812070ed
ashitha <ashithamaryvt@gmail.com>
2023-12-06 18:25:33 +0530

    multiplication

--- calculator.py
+++ calculator.py
@@ -4,10 +4,16 @@ def addtion():
     n2=int(input("Enter the 2nd number: "))
     sum=n1+n2
     print("The sum is : ",sum)
+
 def sqrt():
     n=int(input("Enter a number\n"))
     s=math.sqrt(n)
     print("Square root of ",n," is ",s)
+def multiplication():
+    n1=int(input("Enter the 1st number: "))
+    n2=int(input("Enter the 2nd number: "))
+    mul=n1+n2
+    print("The multiplication is : ",mul)
 
 print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Square-Root\n6.Power\n7.exit\n")
 op=0
@@ -19,7 +25,7 @@ while op<7:
         case 2:
             print("Work In Progress")
         case 3:
-            print("Work In Progress")
+            multiplication()
         case 4:
             print("Work In Progress")
         case 5:
